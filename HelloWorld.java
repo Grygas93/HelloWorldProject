b482bfb0cbc86c49b3516a8584d2bd665340a06a
Ubuntu <ubuntu@ip-172-31-85-118.ec2.internal>
2024-10-24 13:17:51 +0000

    Added a new print line2

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ class HelloWorld
     public static void main(String args[])
     {
         System.out.println("Hello, World");
-        Sustem.out.println("This is new line");
+        System.out.println("This is new line");
 
 
     }
